80c18ea1913191f8d3df14d5e0e8ac3cbf645ede
stevenhughes08 <stevenhughes08@gmail.com>
2021-11-23 12:11:03 -0500

    set custom outputs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
     output: {
         // filename: 'main.js',
-        filename: 'bundle.js',
+        // filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
